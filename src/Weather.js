e813ab603a4369d88ca2f453115c2dca33639d4f
jpaker05 <110342564+jpaker05@users.noreply.github.com>
2023-01-21 10:56:26 -0600

    debugged, added working axios

--- src/Weather.js
+++ src/Weather.js
@@ -1,69 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
-  const weatherData = {
-    city: "New York",
-    temperature: 19,
-    date: "Tuesday 10:00",
-    description: "Cloudy",
-    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
-    humidity: 80,
-    wind: 10,
-  };
+  let [city, setCity] = useState("");
+
+  let [currentWeather, setCurrentWeather] = useState({});
+
+  function showCurrentWeather(response) {
+    setCurrentWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(url).then(showCurrentWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
 
   return (
-    <div className="Weather">
-      <form className="mb-3">
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Type a city.."
-              className="form-control"
-              autoComplete="off"
-            />
-          </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-primary w-100"
-            />
-          </div>
-        </div>
+    <div className="weatherData">
+      <form onSubmit={handleSubmit}>
+        <input type="search" placeholder="Enter a city" onChange={updateCity} />
+        <input type="submit" value="search" />
       </form>
-      <div className="overview">
-        <h1>{weatherData.city}</h1>
-        <ul>
-          <li>Last updated: {weatherData.date}</li>
-          <li>{weatherData.description}</li>
-        </ul>
-      </div>
-      <div className="row">
-        <div className="col-6">
-          <div className="clearfix weather-temperature">
-            <img
-              src={weatherData.imgUrl}
-              alt={weatherData.description}
-              className="float-left"
-            />
-            <div className="float-left">
-              <strong>{weatherData.temperature}</strong>
-              <span className="units">
-                <a href="/">°C</a> | <a href="/">°F</a>
-              </span>
-            </div>
-          </div>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Humidity: {weatherData.humidity}%</li>
-            <li>Wind: {weatherData.wind} km/h</li>
-          </ul>
-        </div>
-      </div>
+
+      <ul>
+        <li>Temperature: {Math.round(currentWeather.temperature)}°C</li>
+        <li>Description: {currentWeather.description} </li>
+        <li>Humidity: {currentWeather.humidity} %</li>
+        <li>Wind: {currentWeather.wind} km/h</li>
+        <li>
+          <img src={currentWeather.icon} alt={currentWeather.description} />
+        </li>
+      </ul>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+
 import reportWebVitals from "./reportWebVitals";
 
 import Weather from "./Weather";
@@ -11,7 +11,8 @@ import "./Weather.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <div className="App">
+    <div>
+      <h1>Weather App</h1>
       <Weather />
     </div>
   </React.StrictMode>
